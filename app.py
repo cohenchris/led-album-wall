cb35b24a3d8fb262b3553793f865ee8537c1fbe0
Chris Cohen <chricohe@qti.qualcomm.com>
2023-12-15 14:00:32 -0800

    initial beta version, should work, will test with my pi + LEDs soon

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import configparser
+import threading
+import time
+import random
 from flask import Flask, request, jsonify, abort
-#fron rpi_ws281x import PixelStrip, Color
+fron rpi_ws281x import PixelStrip, Color
 
 app = Flask(__name__)
 DEBUG = True
 app.debug = True if DEBUG else False
 
-CHECK = lambda ret: abort(500) if not ret else None
 LOG = lambda string: print(string) if DEBUG else None
 
 # LED strip configuration:
-LED_COUNT = 30          # Number of LED pixels.
-LED_PIN = 18            # GPIO pin connected to the data line.
-LED_FREQ_HZ = 800000    # LED signal frequency in hertz (usually 800kHz)
-LED_DMA = 10            # DMA channel to use for generating signal.
-LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest.
-LED_INVERT = False      # True to invert the signal (when using NPN transistor level shift).
-LED_WIPE_INTERVAL = 50  # When wiping all LEDs, this is the delay between wiping each pixel (in ms)
+LED_COUNT = 30            # Number of LED pixels.
+LED_PIN = 18              # GPIO pin connected to the data line.
+LED_FREQ_HZ = 800000      # LED signal frequency in hertz (usually 800kHz)
+LED_DMA = 10              # DMA channel to use for generating signal.
+LED_BRIGHTNESS = 255      # Set to 0 for darkest and 255 for brightest.
+LED_INVERT = False        # True to invert the signal (when using NPN transistor level shift).
+LED_WIPE_INTERVAL_MS = 50 # When wiping all LEDs, this is the delay between wiping each pixel (in ms)
 
 # Initialize LED strips
-#strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-#strip.begin()
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+strip.begin()
 
-NUM_ALBUMS = 8
+# Global variables
 G_LED_STATUS = "off"
 
+# Define ambient RGB thread and stop event
+ambientRgbStopEvent = None
+ambientRgbThread = None
+
+######################################################################
 
 def colorWipe(color, reverse=False):
   """
@@ -36,14 +43,13 @@ def colorWipe(color, reverse=False):
   start, end, step = (strip.numPixels() - 1, -1, -1) if reverse else (0, strip.numPixels, 1)
   
   for i in range(start, end, step):
-    #strip.setPixelColor(i, color)
-    #strip.show()
-    time.sleep(LED_WIPE_INTERVAL / 1000.0)
+    strip.setPixelColor(i, color)
+    strip.show()
+    time.sleep(LED_WIPE_INTERVAL_MS / 1000.0)
 
   LOG("Color wipe succeeded!")
 
-  return True
-
+######################################################################
 
 def turnOn():
   """
@@ -52,21 +58,21 @@ def turnOn():
 
   LOG("Attempting to turn on LEDs...")
 
-  ret = False
+  global G_LED_STATUS
+  global ambientRgbThread
+  global ambientRgbStopEvent
 
-  # Wipe white across the strip
-  #ret = colorWipe(Color(255, 255, 255))
-  ret = True
+  # Turn on RGB Ambient mode, if not already on
+  if not ambientRgbThread and not ambientRgbStopEvent:
+    ambientRgbStopEvent = threading.Event()
+    ambientRgbThread = threading.Thread(target=ledAmbientRgb, args=(ambientRgbStopEvent,))
+    ambientRgbThread.start()
+    LOG("Started ambient RGB.")
 
-  if ret:
-    LED_STATUS = "on"
-    # Turn on RGB Ambient mode
-    ret = ledAmbientRgb()
-
+  G_LED_STATUS = "on"
   LOG("LEDs turned on successfully!")
 
-  return ret
-
+######################################################################
 
 def turnOff():
   """
@@ -75,27 +81,42 @@ def turnOff():
 
   LOG("Attempting to turn off LEDs...")
 
-  ret = False
+  global G_LED_STATUS
+  global ambientRgbThread
+  global ambientRgbStopEvent
 
-  #ret = colorWipe(Color(0, 0, 0), reverse=True)
-  ret = True
+  # Kill ambient RGB
+  if ambientRgbThread and ambientRgbThread.is_alive():
+    ambientRgbStopEvent.set()
+    ambientRgbThread.join()
+    ambientRgbThread = None
+    ambientRgbStopEvent = None
+    LOG("Killed ambient RGB.")
 
-  if ret:
-    LED_STATUS = "off"
+  colorWipe(Color(0, 0, 0), reverse=True)
 
+  G_LED_STATUS = "off"
   LOG("LEDs turned off successfully!")
-  return ret
 
+######################################################################
 
-def ledAmbientRgb():
+def ledAmbientRgb(stopEvent):
   """
      Default ambient RGB mode for LED strips
   """
   
-  LOG("Attempting to start ambient RGB...")
+  LOG("Beginning ambient RGB mode...")
 
-  LOG("Ambient RGB started successfully!")
-  return True
+  while not stopEvent.is_set():
+    LOG("Executing RGB Ambient Wipe")
+    # Wipe to some random color
+    colorWipe((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    # Wipe to some other random color in reverse
+    colorWipe((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), True)
+
+  LOG("Ambient RGB mode stopped successfully!")
+
+######################################################################
 
 def loadConfig():
   """
@@ -104,14 +125,12 @@ def loadConfig():
   """
   LOG("Attempting to load album config...")
 
-  ret = False
-
   config = configparser.ConfigParser()
   config.read("albums.ini")
 
   albums = []
 
-  for i in range(NUM_ALBUMS):
+  for i in range(len(albumDict)):
     albumDict = {}
     keyName = f'Album{i}'
 
@@ -122,13 +141,10 @@ def loadConfig():
 
     albums.append(albumDict)
 
-  print(albums)
-
-  ret = True
-  
   LOG("Album config loaded successfully!")
-  return albums, ret
+  return albums
 
+######################################################################
 
 def highlightAlbum(ledStartIndex, ledEndIndex):
   """
@@ -138,27 +154,24 @@ def highlightAlbum(ledStartIndex, ledEndIndex):
 
   LOG("Attempting to highlight album...")
 
-  ret = False
-
-  ret = ledStartIndex is not None and ledEndIndex is not None
-
-  if ret:
+  if ledStartIndex is not None and ledEndIndex is not None
     for i in range(strip.numPixels()):
       if ledStartIndex <= i <= ledEndIndex:
         # If LED is within range of album to light up, turn it to white
-        #strip.setPixelColor(i, Color(255, 255, 255))
+        strip.setPixelColor(i, Color(255, 255, 255))
+        strip.show()
         pass
       else:
         # If LED is NOT within range, turn it off
-        #strip.setPixelColor(i, Color(0, 0, 0))
+        strip.setPixelColor(i, Color(0, 0, 0))
+        strip.show()
         pass
       
-      #strip.show()
-      #time.sleep(LED_WIPE_INTERVAL / 1000.0)
+      time.sleep(LED_WIPE_INTERVAL_MS / 1000.0)
 
   LOG("Album highlighted successfully!")
-  return ret
 
+######################################################################
 
 def findPossibleAlbumMatch(artistName, albumName):
   """
@@ -169,24 +182,35 @@ def findPossibleAlbumMatch(artistName, albumName):
   LOG("Attempting to find an album match...")
 
   found = False
-  ret = False
+  global ambientRgbThread
+  global ambientRgbStopEvent
   
   # This loads the provided albums.ini file, and returns them in an array of dictionaries
-  wallAlbums, ret = loadConfig()
+  wallAlbums = loadConfig()
   
-  if ret:
-    # Search for a match within albums on the wall
-    for album in wallAlbums:
-        # If there is a match, highlight the album on the wall
-        if album["artistName"] == artistName and album["albumName"] == albumName:
-            found = True
-            ret = highlightAlbum(album["ledStartIndex"], album["ledEndIndex"])
-            break
+  # Search for a match within albums on the wall
+  for album in wallAlbums:
+      # If there is a match, highlight the album on the wall
+      LOG(f"Checking match against {album["albumName"]} by {album["artistName"}")
+      if album["artistName"] == artistName and album["albumName"] == albumName:
+          LOG("Match found!")
+          found = True
+          # First, stop ambient RGB if it is running
+          if ambientRgbThread and ambientRgbThread.is_alive():
+            ambientRgbStopEvent.set()
+            ambientRgbThread.join()
+            ambientRgbThread = None
+            ambientRgbStopEvent = None
+            LOG("Killed ambient RGB.")
+          # Highlight album on wall
+          highlightAlbum(int(album["ledStartIndex"]), int(album["ledEndIndex"]))
+          break
+      LOG("No match, continuing search...")
 
-  ret = True
-  LOG("Album match search attempt succeeded!")
-  return found, ret
+  LOG("Album match search finished!")
+  return found
 
+######################################################################
 
 @app.route("/albumWall", methods=['POST'])
 def albumWall():
@@ -196,25 +220,23 @@ def albumWall():
 
   LOG("Processing request...")
 
-  ret = False
-
   data = request.get_json()
   
   # Update the LED strips based on the provided data
   
   # If the provided status is "off", turn LEDs off, then quit
-  led_status = data.get("led_status")
+  ledStatus = data.get("ledStatus")
   
   # Turn off LEDs
-  if led_status == "off":
-    if led_status != G_LED_STATUS:
-      print("Turning off LEDs...")
-      ret = turnOff()
-      CHECK(ret)
-      print("LEDs turned off!")
-  
+  if ledStatus == "off":
+    LOG("Attempting to turn off LEDs...")
+    if ledStatus != G_LED_STATUS:
+      LOG("LEDs on, turning off...")
+      turnOff()
+    LOG("LEDs turned off!")
+
   # Turn on LEDs
-  elif led_status == "on":
+  elif ledStatus == "on":
     # First, we should determine if there's a match. If there is, then we should highlight that album.
     # The idea is that, if the lights are off, we don't want to turn them all on, THEN highlight the album.
     # If there's a match, we should highlight the album. If not, we should execute the normal turnOn function.
@@ -223,17 +245,14 @@ def albumWall():
     artistName = data.get("artistName")
     albumName = data.get("albumName")
 
-    albumFound, ret = findPossibleAlbumMatch(artistName, albumName)
-    CHECK(ret)
+    albumFound = findPossibleAlbumMatch(artistName, albumName)
 
     if not albumFound:
       ret = turnOn()
-      CHECK(ret)
+
   else:
     abort(500)
 
   LOG("Request processed successfully!")
   return jsonify({"message": "Success!"}), 200
 
-if __name__ == "__main__":
-    app.run()
